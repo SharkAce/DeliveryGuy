29a19366f5441abd42a1205c9647b5f602ec1b46
SharkAce <85720638+SharkAce@users.noreply.github.com>
2026-08-21 12:40:30 -0400

    Simple Setup

---
+++ Assets/Scripts/WaypointController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WaypointType
+{
+    Building,
+    Parking,
+    Intersection
+}
+
+public class WaypointController : MonoBehaviour
+{
+    public WaypointType type;
+    public List<WaypointController> nextWaypoints;
+}
