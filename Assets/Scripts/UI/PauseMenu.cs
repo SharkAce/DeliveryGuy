8488bc7ea485df731852c3f602fa7247c329b36d
aygurs <aydin_gurs@hotmail.com>
2026-08-24 23:03:20 +0100

    Add directional arrow, phone UI and pause UI

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Menu Panels")]
+    [SerializeField] private GameObject pauseOverlay;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject howToPlayPanel;
+
+    private bool isPaused;
+
+    private void Start()
+    {
+        isPaused = false;
+
+        pauseOverlay.SetActive(false);
+        pausePanel.SetActive(true);
+        howToPlayPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+
+        pauseOverlay.SetActive(true);
+        pausePanel.SetActive(true);
+        howToPlayPanel.SetActive(false);
+
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+
+        pauseOverlay.SetActive(false);
+        pausePanel.SetActive(true);
+        howToPlayPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
+    public void ShowHowToPlay()
+    {
+        pausePanel.SetActive(false);
+        howToPlayPanel.SetActive(true);
+    }
+
+    public void ShowPauseMenu()
+    {
+        howToPlayPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
+}
