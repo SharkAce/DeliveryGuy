996992e27dd612f17eb37e36457704bc88a50f31
SharkAce <85720638+SharkAce@users.noreply.github.com>
2026-08-28 14:25:55 -0400

    Add spawning npc cars

--- Assets/Scripts/NpcCarControls.cs
+++ Assets/Scripts/NpcCarControls.cs
@@ -6,7 +6,6 @@ public class NpcCarControls : MonoBehaviour
 {
     [SerializeField] private float maxAISpeed = 10f;
     [SerializeField] private WaypointController currentWaypoint = null;
-    [SerializeField] private WaypointController previousWaypoint = null;
     [SerializeField] private float waypointTriggerDist = 1f;
     private float currentSpeed = 0;
     private CarController.Controls controls = new CarController.Controls();
@@ -31,10 +30,12 @@ public class NpcCarControls : MonoBehaviour
     {
         if (startWp == null || previousWp == null) return;
         currentWaypoint = startWp;
-        previousWaypoint = previousWp;
 
         // Select a random point between the two waypoints
         transform.position = Vector3.Lerp(startWp.transform.position, previousWp.transform.position, Random.value);
+        Vector2 directionToWaypoint = (startWp.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(directionToWaypoint.y, directionToWaypoint.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
     void ApplyControls()
     {
@@ -45,25 +46,17 @@ public class NpcCarControls : MonoBehaviour
         float targetSpeed = maxAISpeed * dot;
 
         controls.driveInput = currentSpeed < targetSpeed ? 1f : 0f;
-        controls.brakeInput = currentSpeed > targetSpeed ? 0f : 1f;
-        controls.steerInput = cross;
+        controls.brakeInput = currentSpeed > targetSpeed ? 1f : 0f;
+        controls.steerInput = Mathf.Clamp(cross, -1f, 1f);
 
         car.ApplyControls(controls);
 
         if (Vector3.Distance(currentWaypoint.transform.position, transform.position) < waypointTriggerDist) 
-            SelectAINextWaypoint();
+            SelectNextWaypoint();
     }
 
-    void SelectAINextWaypoint()
+    void SelectNextWaypoint()
     {
-        foreach (WaypointController wp in currentWaypoint.nextWaypoints)
-        {
-            if (wp != currentWaypoint && wp != previousWaypoint)
-            {
-                previousWaypoint = currentWaypoint;
-                currentWaypoint = wp;
-                break;
-            }
-        }
+        currentWaypoint = currentWaypoint.nextWaypoints[Random.Range(0, currentWaypoint.nextWaypoints.Count)];
     }
 }
